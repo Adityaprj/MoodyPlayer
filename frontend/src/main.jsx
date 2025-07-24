2a5b1bc269f495bb2d1b4177a6e49fc356e2e1e0
Aditya Kumar <araj22073@gmail.com>
2025-07-24 21:48:04 +0530

    db added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import mongoose from 'mongoose'
+
+mongoose.connect('mongodb://localhost:27017/mydatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
